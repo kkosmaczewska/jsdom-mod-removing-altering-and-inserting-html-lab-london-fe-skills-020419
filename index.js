502c83cfa6fe95c1c4fe50be225bcc6ca06e5910
Kasia Kosmaczewska <kakosmaczewska@gmail.com>
2019-03-04 19:21:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ main.remove()
 
 
 /* Create your new element here and assign it to newHeader */
-var element = document.createElement('h1')
-document.body.appendChild(H1)
 const newHeader = document.createElement('h1');
+document.body.appendChild(H1)
