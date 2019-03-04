c30a82f1543a82bbc912116137d6693beff3e958
Kasia Kosmaczewska <kakosmaczewska@gmail.com>
2019-03-04 19:16:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ main.remove()
 
 
 /* Create your new element here and assign it to newHeader */
-var element = document.createElement('H1 id="victory"')
+var element = document.createElement('H1')
 document.body.appendChild(element)
 element.innerHTML = "Kasia is the champion!"
 const newHeader = H1;
