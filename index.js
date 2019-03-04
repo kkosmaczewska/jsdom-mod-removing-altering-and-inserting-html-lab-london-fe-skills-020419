0720c748069305877eebeb6600be72235e869280
Kasia Kosmaczewska <kakosmaczewska@gmail.com>
2019-03-04 19:09:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 /* Enter the code to remove the main node element under this comment */
+main.remove()
+
 
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+var element = document.createElement('H1 id="victory"')
+document.h1.appendChild(element)
+const newHeader = "Kasia is the champion!";
